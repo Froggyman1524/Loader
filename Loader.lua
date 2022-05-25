01a193f44b949678e3b3bc63a8c1b01f9fd476a2
Froggyman1524 <82377105+Froggyman1524@users.noreply.github.com>
2022-05-25 20:34:54 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,10 @@
 local Obby = 6429911702
+local Border = 7014716500
 
 if game.PlaceId == Obby then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Froggyman1524/Roblox-TrollGui/main/TrollGui.lua"))()
+    elseif game.PlaceId == Border then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Froggyman1524/ProbordeScript/main/Protected%20(1).lua"))()
     else
         
 end
