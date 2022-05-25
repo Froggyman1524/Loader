2bbe466927fbdffe9d8a6af0dfa6cc44095c3470
Froggyman1524 <82377105+Froggyman1524@users.noreply.github.com>
2022-05-25 09:41:12 +0700

    Loader

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+local Obby = 6429911702
+
+if game.PlaceId == Obby then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Froggyman1524/Roblox-TrollGui/main/TrollGui.lua"))()
+    else
+        
+end
