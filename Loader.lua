0dc11c9afc2542f989b3373a3d842ce7ff75a94b
Froggyman1524 <82377105+Froggyman1524@users.noreply.github.com>
2022-05-27 21:14:51 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,13 @@
 local Obby = 6429911702
 local Border = 7014716500
+local ClickSim = 7560156054
 
 if game.PlaceId == Obby then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Froggyman1524/Roblox-TrollGui/main/TrollGui.lua"))()
     elseif game.PlaceId == Border then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Froggyman1524/ProbordeScript/main/Protected%20(1).lua"))()
+    elseif game.PlaceId == ClickSim then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Froggyman1524/ClickSimScript/main/ClickerSimSource.lua"))()
     else
         
 end
